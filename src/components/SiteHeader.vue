196df8cd53b8fa9464acac23e9457c283552fb4a
MartinaDR1 <derosemartina933@gmail.com>
2023-04-13 16:56:22 +0200

    details header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,36 +1,38 @@
 <script>
-  export default{
-    data(){
-      return {
-        name: 'SiteHeader'
-      }
-    }
-  }
+import navBar from "../data/navBar";
+export default {
+    data() {
+        return {
+            name: 'SiteHeader',
+            navBar: navBar,
+        }
+    },
+    components: {
+        navBar,
+    },
+}
 </script>
 
 <template>
-  <header>
-    <div class="container my-4">
-      <div class="row">
-        <div class="col d-flex justify-content-between">
-          <div class="logo">
-            <img src="../assets/img/dc-logo.png" alt="">
-          </div>
+    <header>
+        <div class="container my-4">
+            <div class="row">
+                <div class="col d-flex justify-content-between">
+                    <div class="logo">
+                        <img style="width: 65px;" src="../assets/img/dc-logo.png" alt="">
+                    </div>
 
-          <div class="nav">
-            <ul class="d-flex">
-              <li class="mx-3">lorem</li>
-              <li class="mx-3">lorem</li>
-              <li class="mx-3">lorem</li>
-              <li class="mx-3">lorem</li>
-              <li class="mx-3">lorem</li>
-            </ul>
-          </div>
+                    <div class="nav d-flex align-items-center ">
+                        <ul>
+                            <li>
+                                <a href="" v-for="item in navBar">{{item.text }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </header>
+    </header>
 </template>
 
-<style>
-</style>
+<style></style>
---
+++ src/data/navBar.js
@@ -0,0 +1,32 @@
+export default [
+    {
+      text: 'Characters',
+    },
+    {
+      text: 'Comics',
+    },
+    {
+      text: 'Movies',
+    },
+    {
+      text: 'Tv',
+    },
+    {
+      text: 'Games',
+    },
+    {
+      text: 'Collectibles',
+    },
+    {
+      text: 'Videos',
+    },
+    {
+      text: 'Fans',
+    },
+    {
+      text: 'News',
+    },
+    {
+      text: 'Shop',
+    },
+];
