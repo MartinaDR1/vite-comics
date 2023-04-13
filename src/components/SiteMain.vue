12d69f09208edbe444960c722cdd2975b03ee7a4
MartinaDR1 <derosemartina933@gmail.com>
2023-04-13 17:32:51 +0200

    add main

---
+++ src/components/SiteMain.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'SiteMain'
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <span>Content goes here</span> 
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    @use "../assets/partials/SiteMain.scss" as *;
+</style>
