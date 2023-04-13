a2b809f18c52c948fe752fb668caa49240c528e8
MartinaDR1 <derosemartina933@gmail.com>
2023-04-13 17:54:26 +0200

    add banner

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,17 +1,33 @@
 <script>
+import banner from "../data/banner";
 export default {
     data() {
         return {
-            name: 'SiteMain'
+            name: 'SiteMain',
+            banner: banner,
         }
-    }
+    },
+    components: {
+        banner,
+    },
 }
 </script>
 
 <template>
     <main>
-        <div class="container">
+        <div class="container py-5">
             <span>Content goes here</span> 
+        </div>
+
+        <div class="banner py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col d-flex gap-3 justify-content-center align-items-center" v-for="item in banner">
+                        <img :src="item.icon" alt="">
+                        <span class="px-2">{{ item.text }}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 </template>
---
+++ src/data/banner.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        text: 'Digital Comics',
+        icon: '/src/assets/img/buy-comics-digital-comics.png',
+    },
+    {
+        text: 'DC Merchandise',
+        icon: '/src/assets/img/buy-comics-merchandise.png',
+    },
+    {
+        text: 'Subscription',
+        icon: '/src/assets/img/buy-comics-subscriptions.png',
+    },
+    {
+        text: 'Comic Shop Locator',
+        icon: '/src/assets/img/buy-comics-shop-locator.png',
+    },
+    {
+        text: 'DC Power Visa',
+        icon: '/src/assets/img/buy-dc-power-visa.svg',
+    },
+]
